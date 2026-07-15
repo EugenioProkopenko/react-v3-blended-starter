cc252fea7590bca1aaa24bfbe2e764544c06a22d
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2026-07-15 15:31:46 +0200

    first task

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,26 @@
-import Section from "../Section/Section";
-import Container from "../Container/Container";
+import Section from '../Section/Section';
+import Container from '../Container/Container';
+import { useEffect } from 'react';
+import { getPhotos } from '../../services/photos';
+import Form from '../Form/Form';
 
 export default function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await getPhotos('animal');
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
-        Home page
+        <Container>Home page</Container>
+        <Form />
       </Section>
     </>
   );
