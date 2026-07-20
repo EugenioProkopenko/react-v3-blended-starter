46972903dd9225a6b8168bab697baae2daac9cdc
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2026-07-20 14:39:51 +0200

    third task

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,21 @@ import Container from '../Container/Container';
 
 import { getPhotos } from '../../services/photos';
 import Form from '../Form/Form';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import PhotosGallery from '../PhotosGallery/PhotosGallery';
+import { useState } from 'react';
+import type { Photo } from '../../types/photo';
 
 export default function App() {
+  const [photos, setPhotos] = useState<Photo[] | null>(null);
   const handleSabmit = async (query: string) => {
     try {
       const data = await getPhotos(query);
+      if (data.length === 0) {
+        toast.error('По вашому запиту нічого не знайдено');
+        return;
+      }
+      setPhotos(data);
       console.log(data);
     } catch (error) {
       console.log(error);
@@ -19,6 +28,7 @@ export default function App() {
       <Section>
         <Container>
           <Form onSabmit={handleSabmit} />
+          {photos && photos.length > 0 && <PhotosGallery photos={photos} />}
           <Toaster />
         </Container>
       </Section>
--- src/components/PhotosGallery/PhotosGallery.tsx
+++ src/components/PhotosGallery/PhotosGallery.tsx
@@ -1,5 +1,17 @@
-import Grid from "../Grid/Grid";
+import type { Photo } from '../../types/photo';
+import Grid from '../Grid/Grid';
+import PhotosGalleryItem from '../PhotosGalleryItem/PhotosGalleryItem';
 
-export default function PhotosGallery() {
-  return <Grid>{/* PhotosGalleryItems */}</Grid>;
+interface PhotosGalleryProps {
+  photos: Photo[];
+}
+
+export default function PhotosGallery({ photos }: PhotosGalleryProps) {
+  return (
+    <Grid>
+      {photos.map(item => (
+        <PhotosGalleryItem photo={item} key={item.id} />
+      ))}
+    </Grid>
+  );
 }
