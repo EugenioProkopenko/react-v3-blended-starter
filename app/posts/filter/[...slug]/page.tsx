f81c749268d217f9954a231bf17ef387b0fe29b6
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2026-08-11 19:34:28 +0200

    Add prefech

---
+++ app/posts/filter/[...slug]/page.tsx
@@ -0,0 +1,30 @@
+ import { fetchPosts } from '@/lib/api';
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import PostsClient from './Posts.client';
+ interface PostsPageProps{
+  params: Promise<{slug: string[]}>
+ }
+
+export default async function PostsPage({params}: PostsPageProps) {
+  const {slug} = await params
+  const userId = slug[0]
+  const queryClient = new QueryClient();
+  const searchQuery = "";
+  const currentPage = 1
+   await queryClient.prefetchQuery({
+      queryKey: ['posts', searchQuery, currentPage, userId],
+    queryFn: () =>
+      fetchPosts({
+        searchText: searchQuery,
+        page: currentPage,
+        ...(userId !== 'All' && { userId }),
+      }),
+  }
+   )
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <PostsClient userId={userId}/>
+    </HydrationBoundary>
+  );
+}
