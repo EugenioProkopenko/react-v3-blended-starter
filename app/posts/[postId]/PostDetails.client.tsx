1d95f5f153646b6fd8a24a3bda5bc3c1030a44ae
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2026-08-11 21:17:27 +0200

    Add PostDatails.client

---
+++ app/posts/[postId]/PostDetails.client.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchPostById, fetchUserById } from '@/lib/api';
+
+import css from './PostDetails.module.css';
+import { useEffect, useState } from 'react';
+import { User } from '@/types/user';
+
+
+export default function PostDetailsClient() {
+  const router = useRouter()
+
+
+    const {postId} = useParams<{postId: string}>();
+
+     const {data} = useQuery({
+      queryKey: ['post', postId],
+       queryFn: () => fetchPostById(postId),
+       refetchOnMount: false
+     })
+
+
+  const handleClickBack = () => {
+    router.back()
+
+
+
+
+  };
+  const [user, setUser] = useState<User | null>(null)  
+
+  useEffect(() => {
+
+    if(!data) return
+    const fn = async () => {
+      const response = fetchUserById(data?.userId)
+      setUser(response)
+    };
+    fn();
+  }, []);
+
+  return (
+    <>
+      <main className={css.main}>
+        <div className={css.container}>
+          <div className={css.item}>
+            <button className={css.backBtn} onClick={handleClickBack}>← Back</button>
+
+            <div className={css.post}>
+              <div className={css.wrapper}>
+                <div className={css.header}>
+                  <h2>{data?.title}</h2>
+                </div>
+
+                <p className={css.content}>{data?.body}</p>
+              </div>
+              <p className={css.user}>Author: User name</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,21 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import PostDetailsClient from './PostDetails.client';
+import { fetchPostById } from '@/lib/api';
+interface PostDetailsProps {
+  params: Promise<{postId: string}>
+}
+export default async function PostDetails({ params }: PostDetailsProps) {
+  const { postId } = await params
+  
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['post', postId],
+    queryFn: () => fetchPostById(postId)
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <PostDetailsClient />
+    </HydrationBoundary>
+  );
+}
